a7ac87aab8371f79c282c3ae87fb60cf90f07059
Troy James Palanca <tjpalanca@outlook.com>
2015-07-09 22:22:08 +0800

    Made further corrections. Added path of least resistance plot

--- Script/S4 - Path of Least Resistance.R
+++ Script/S4 - Path of Least Resistance.R
@@ -19,6 +19,7 @@ library(geosphere)
 load("Data/D1 - Enrollment Data.RData")
 load("Data/D2 - Schools Data.RData")
 load("Data/D3 - CityMuni Data.RData")
+schools.dt <- schools.dt %>% mutate(school.citymuni = paste(school.citymuni, school.province))
 
 PH.map <- get_googlemap (
   center = c(121.8347,12.45113),
@@ -35,7 +36,7 @@ PH.map <- get_googlemap (
 for (i in 1:length(schools.dt$school.citymuni[is.na(schools.dt$map.lat)])) {
   distances.vc <- stringdist(
     tolower((schools.dt$school.citymuni[is.na(schools.dt$map.lat)])[i]),
-    tolower(citymuni.dt$citymuni))
+    tolower(paste(citymuni.dt$citymuni, citymuni.dt$province)))
   schools.dt$matched.citymuni[is.na(schools.dt$map.lat)][i] <-
     citymuni.dt$citymuni[distances.vc == min(distances.vc)][1]
   schools.dt$matched.map.lat[is.na(schools.dt$map.lat)][i] <-
@@ -98,20 +99,44 @@ for (i in 1:nrow(citymuni.dt)){
               format(i/nrow(citymuni.dt), digits = 2)))
 }
 
+rm(distances.vc, i)
+
 # Plot of Paths -----------------------------------------------------------
-paths.dt <- citymuni.dt %>% select(citymuni, map.lat, map.lon,
-                                   nearest.elementary.school.lat,
-                                   nearest.elementary.school.lon,
-                                   nearest.secondary.school.lat,
-                                   nearest.secondary.school.lon) %>%
-  melt(id.var = "citymuni", variable.name = "stage", value.name = "coordinate")
+paths.dt <- citymuni.dt %>% mutate(citymuniprov = paste(citymuni, province)) %>%
+  select(citymuniprov, map.lat, map.lon,
+         nearest.elementary.school.lat,
+         nearest.elementary.school.lon,
+         nearest.secondary.school.lat,
+         nearest.secondary.school.lon) %>%
+  melt(id.var = "citymuniprov", variable.name = "stage", value.name = "coordinate")
 
 paths.dt <- cbind(paths.dt, do.call("rbind", strsplit(as.character(paths.dt$stage), "\\."))) %>%
   mutate(type_coord = ifelse(`1` == "map", as.character(`2`), as.character(`4`)),
          level = ifelse(`1` == "map", "origin", as.character(`2`))) %>%
   select(-`1`, -`2`, -`3`, -`4`, -stage) %>%
-  dcast(citymuni + level ~ type_coord, value.var = "coordinate")
+  dcast(citymuniprov + level ~ type_coord, value.var = "coordinate")
 
+paths.dt$level <- factor(as.character(paths.dt$level),
+                         levels = c("origin", "elementary", "secondary"))
+
+paths.dt <- paths.dt %>% arrange(citymuniprov, level) %>%
+  left_join(citymuni.dt %>% mutate(citymuniprov = paste(citymuni, province)) %>%
+              select(citymuniprov, nearest.elementary.school.dist,
+                     nearest.secondary.school.dist))
+
+ggmap(PH.map, base_layer = ggplot(paths.dt, aes(x = lon, y = lat))) +
+  geom_path(aes(group = citymuniprov)) +
+  coord_map(xlim = c(116.812721, 126.856628), ylim = c(4.46811, 21.23415)) +
+  ggtitle("Secondary") +
+  theme(legend.position = "bottom",
+        axis.title = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        text = element_text(family = "Open Sans"),
+        plot.title = element_text(hjust = 0, family = "Raleway", face = "bold"),
+        plot.background = element_rect(color = NA, fill = "grey98"),
+        legend.background = element_blank(),
+        strip.text = element_text(size = 14))
 
 
 # Plot of Distances -------------------------------------------------------
