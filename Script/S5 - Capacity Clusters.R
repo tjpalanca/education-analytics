f70ecd8cae6fdfbba2afa6d7655894e6ae33652d
tjpalanca <tjpalanca@outlook.com>
2015-07-12 19:14:20 +0800

    [WIP] Added exploratory plots. Discovered outliers.

--- Script/S5 - Capacity Clusters.R
+++ Script/S5 - Capacity Clusters.R
@@ -3,14 +3,73 @@
 # July 10, 2015
 
 # Libraries ---------------------------------------------------------------
-
+library(dplyr)
+library(reshape2)
+library(ggplot2)
 
 # Data --------------------------------------------------------------------
+load("Data/D4 - Schools Data Expanded.RData")
 load("Data/D1 - Enrollment Data.RData")
-load("Data/D2 - Schools Data.RData")
-load("Data/D3 - CityMuni Data.RData")
 
-# Hello -------------------------------------------------------------------
+# Metric Construction ------------------------------------------------------
+enrolment.dt <- enrolment.dt %>% group_by(school.id, year) %>%
+  summarise(enrolment = sum(enrollment)) %>% ungroup() %>%
+  dcast(school.id ~ year) %>%
+  rename(enrollment.2012 = `2012`,
+         enrollment.2013 = `2013`,
+         enrollment.2014 = `2014`,
+         enrollment.2015 = `2015`)
+
+schools.dt <- schools.dt %>% left_join(enrolment.dt)
+rm(enrolment.dt)
+
+schools.dt <- schools.dt %>% mutate(
+  all.teacher.ratio =
+    (teachers.instructor + teachers.mobile + teachers.regular + teachers.sped)/
+    enrollment.2014,
+  regular.teacher.ratio =
+    (teachers.regular + teachers.sped) / enrollment.2014,
+  academic.room.ratio =
+    (rooms.standard.academic + rooms.nonstandard.academic) / enrollment.2013,
+  standard.room.ratio =
+    (rooms.standard.academic) / enrollment.2013,
+  full.room.ratio =
+    (rooms.standard.academic + rooms.standard.unused +
+     rooms.nonstandard.academic + rooms.nonstandard.unused) / enrollment.2013,
+  mooe.ratio = school.mooe / enrollment.2015
+)
+
+# Checking Plots ----------------------------------------------------------
+
+ggplot(schools.dt, aes(x = all.teacher.ratio)) +
+  geom_histogram(binwidth = 0.005) +
+  geom_rug() +
+  coord_cartesian(xlim = c(0,0.15))
+
+ggplot(schools.dt, aes(x = regular.teacher.ratio)) +
+  geom_histogram(binwidth = 0.005) +
+  geom_rug() +
+  coord_cartesian(xlim = c(0,0.15))
+
+ggplot(schools.dt, aes(x = academic.room.ratio)) +
+  geom_histogram(binwidth = 0.005) +
+  geom_rug() +
+  coord_cartesian(xlim = c(0,0.1))
+
+ggplot(schools.dt, aes(x = standard.room.ratio)) +
+  geom_histogram(binwidth = 0.005) +
+  geom_rug() +
+  coord_cartesian(xlim = c(0,0.1))
+
+ggplot(schools.dt, aes(x = full.room.ratio)) +
+  geom_histogram(binwidth = 0.005) +
+  geom_rug() +
+  coord_cartesian(xlim = c(0,0.1))
+
+ggplot(schools.dt, aes(x = mooe.ratio)) +
+  geom_histogram(binwidth = 100) +
+  geom_rug() +
+  coord_cartesian(xlim = c(0,5000))
 
 
 # To Do:
