fa6e2108ff31eec2119d5cf2c2f1bb1816c982e9
tjpalanca <tjpalanca@outlook.com>
2015-07-13 16:46:56 +0800

    Created boxplots and histograms for capacity measures

--- Script/S5 - Capacity Clusters.R
+++ Script/S5 - Capacity Clusters.R
@@ -7,6 +7,8 @@ library(dplyr)
 library(reshape2)
 library(ggplot2)
 library(gridExtra)
+library(extrafont)
+loadfonts()
 
 # Data --------------------------------------------------------------------
 load("Data/D4 - Schools Data Expanded.RData")
@@ -41,67 +43,83 @@ schools.dt <- schools.dt %>% mutate(
 )
 
 # Standardization of schools ------------------------------------------------
-schools.dt %>%
-
+schools.dt <- schools.dt %>%
+  filter(is.na(all.teacher.ratio) + is.na(regular.teacher.ratio) +
+           is.na(academic.room.ratio) + is.na(standard.room.ratio) +
+           is.na(full.room.ratio) + is.na(mooe.ratio) == 0)
 
 # Checking Plots ----------------------------------------------------------
 
-grid.arrange(ggplot(schools.dt, aes(x = all.teacher.ratio)) +
-  geom_histogram(binwidth = 0.005) +
-  geom_rug() +
-  coord_cartesian(xlim = c(0,0.1)),
+histtheme.tm <-
+  theme_minimal() +
+  theme(text = element_text(family = "Open Sans"),
+        axis.title.y = element_blank(),
+        axis.title.x = element_text(face = "bold"))
 
-ggplot(schools.dt, aes(x = regular.teacher.ratio)) +
-  geom_histogram(binwidth = 0.005) +
-  geom_rug() +
-  coord_cartesian(xlim = c(0,0.1)),
+grid.arrange(
+  ggplot(schools.dt, aes(x = 1/all.teacher.ratio)) +
+    geom_histogram(binwidth = 1, color = NA, fill = "darkgreen") +
+    geom_rug() + xlab("Student to Teacher Ratio") +
+    scale_x_reverse() +
+    coord_cartesian(xlim = c(0,100)) +
+    histtheme.tm,
+  ggplot(schools.dt, aes(x = 1/regular.teacher.ratio)) +
+    geom_histogram(binwidth = 1, color = NA, fill = "darkgreen") +
+    geom_rug() + xlab("Regular Teachers to Student Ratio") +
+    scale_x_reverse() +
+    coord_cartesian(xlim = c(0,100)) +
+    histtheme.tm,
+  ggplot(schools.dt, aes(x = mooe.ratio)) +
+    geom_histogram(binwidth = 20,  color = NA, fill = "darkblue") +
+    geom_rug() + xlab("MOOE to Student Ratio") +
+    coord_cartesian(xlim = c(0,2000)) +
+    histtheme.tm,
+  ggplot(schools.dt, aes(x = 1/academic.room.ratio)) +
+    geom_histogram(binwidth = 1, color = NA, fill = "darkorange") +
+    scale_x_reverse() +
+    geom_rug() + xlab("Student to Academic Room Ratio") +
+    coord_cartesian(xlim = c(0,100)) +
+    histtheme.tm,
+  ggplot(schools.dt, aes(x = 1/standard.room.ratio)) +
+    geom_histogram(binwidth = 1, color = NA, fill = "darkorange") +
+    scale_x_reverse() +
+    geom_rug() + xlab("Student to Standard Room Ratio") +
+    coord_cartesian(xlim = c(0,100)) +
+    histtheme.tm,
+  ggplot(schools.dt, aes(x = 1/full.room.ratio)) +
+    geom_histogram(binwidth = 1, color = NA, fill = "darkorange") +
+    scale_x_reverse() +
+    geom_rug() + xlab("Student to All Rooms Ratio") +
+    coord_cartesian(xlim = c(0,100)) +
+    histtheme.tm,
+  ncol = 3, main = "Capacity Metrics\n")
 
-ggplot(schools.dt, aes(x = academic.room.ratio)) +
-  geom_histogram(binwidth = 0.005) +
-  geom_rug() +
-  coord_cartesian(xlim = c(0,0.075)),
+grid.arrange(
+  ggplot(schools.dt %>% select(school.id, regular.teacher.ratio, all.teacher.ratio) %>%
+           melt(id.var = "school.id", variable.name = "metric", value.name = "value"),
+         aes(x = metric, y = 1/value)) +
+    geom_boxplot() +
+    coord_cartesian(ylim = c(0,100)),
+  ggplot(schools.dt %>% select(school.id, standard.room.ratio, academic.room.ratio,
+                               full.room.ratio) %>%
+           melt(id.var = "school.id", variable.name = "metric", value.name = "value"),
+         aes(x = metric, y = 1/value)) +
+    geom_boxplot() +
+    coord_cartesian(ylim = c(0,100)),
+  ggplot(schools.dt %>% select(school.id, mooe.ratio) %>%
+           melt(id.var = "school.id", variable.name = "metric", value.name = "value"),
+         aes(x = metric, y = value)) +
+    geom_boxplot() +
+    coord_cartesian(ylim = c(0,2000)),
+  ncol = 3
+)
 
-ggplot(schools.dt, aes(x = standard.room.ratio)) +
-  geom_histogram(binwidth = 0.005) +
-  geom_rug() +
-  coord_cartesian(xlim = c(0,0.075)),
+# It is quite obvious that outliers exist, and these shall be examined separately. For plotting purposes, we winsorize the variables at the 1st and 99th percentile.
 
-ggplot(schools.dt, aes(x = full.room.ratio)) +
-  geom_histogram(binwidth = 0.005) +
-  geom_rug() +
-  coord_cartesian(xlim = c(0,0.075)),
 
-ggplot(schools.dt, aes(x = mooe.ratio)) +
-  geom_histogram(binwidth = 100) +
-  geom_rug() +
-  coord_cartesian(xlim = c(0,2000)),
 
-ncol = 2, main = "Capacity Metrics")
 
-ggplot(schools.dt %>% select(school.id, regular.teacher.ratio, all.teacher.ratio) %>%
-         melt(id.var = "school.id", variable.name = "metric", value.name = "value"),
-       aes(x = metric, y = value)) +
-  geom_violin()
 
-ggplot(schools.dt %>% select(school.id, standard.room.ratio, academic.room.ratio,
-                             full.room.ratio) %>%
-         melt(id.var = "school.id", variable.name = "metric", value.name = "value"),
-       aes(x = metric, y = value)) +
-  geom_violin() +
-  coord_cartesian(ylim = c(0,0.075))
-
-ggplot(schools.dt %>% select(school.id, standard.room.ratio, academic.room.ratio,
-                             full.room.ratio) %>%
-         melt(id.var = "school.id", variable.name = "metric", value.name = "value"),
-       aes(x = metric, y = value)) +
-  geom_violin() +
-  coord_cartesian(ylim = c(0,0.075))
-
-ggplot(schools.dt %>% select(school.id, mooe.ratio) %>%
-         melt(id.var = "school.id", variable.name = "metric", value.name = "value"),
-       aes(x = metric, y = value)) +
-  geom_boxplot() +
-  coord_cartesian(ylim = c(0,2000))
 
 # To Do:
 # 4. Compute for capacity metrics and cluster the schools
