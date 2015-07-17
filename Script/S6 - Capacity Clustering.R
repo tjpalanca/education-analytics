380c66f5f82ef0b00a7ae5a90019b913cdd8ed5c
tjpalanca <tjpalanca@outlook.com>
2015-07-17 16:15:30 +0800

    Capacity Clustering Script initialize

--- Script/S6 - Capacity Clustering.R
+++ Script/S6 - Capacity Clustering.R
@@ -2,3 +2,12 @@
 # S5 - Capacity Outlier Analysis and Clustering
 # July 16, 2015
 
+# Libraries ---------------------------------------------------------------
+
+
+# Data --------------------------------------------------------------------
+load("Data/D5 - Capacity Data.RData")
+
+# Hierarchical Clustering -------------------------------------------------
+
+
