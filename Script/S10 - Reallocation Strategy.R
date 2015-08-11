7f81d6c58bc7d23b3d8122c36b4ab86abdcf6465
tjpalanca <tjpalanca@outlook.com>
2015-08-11 11:06:40 +0800

    Explores changes in the ratios from using nonstandard and vacant rooms.

--- Script/S10 - Reallocation Strategy.R
+++ Script/S10 - Reallocation Strategy.R
@@ -1,11 +1,34 @@
 # Data Analytics for Education
-# S10 - Reallocation Strategy
+# S10 - Reallocation Strategies
 # August 10, 2015
 # Author: Troy James R Palanca
 
+# This script explores possible reallocation strategies to improve capacity within the current
+# resource constraints.
+
 # Libraries ---------------------------------------------------------------
 library(dplyr)
+library(ggplot2)
 
 # Data --------------------------------------------------------------------
+load("Data/D5 - Capacity Data.RData")
 
+# Maximizing Room Use ------------------------------------------------------
+
+# Distribution of change in ratio from using vacant rooms
+ggplot(schools.dt %>%
+         filter(!(full.room.ratio == academic.room.ratio)),
+       aes(x = (1/academic.room.ratio - 1/full.room.ratio))) +
+  geom_density(aes(fill = school.classification), color = NA, alpha = 0.5) +
+  scale_x_log10()
+
+# Distribution of change in ratio from using nonstandard rooms
+ggplot(schools.dt %>%
+         filter(!(standard.room.ratio == academic.room.ratio)),
+       aes(x = (1/standard.room.ratio - 1/academic.room.ratio))) +
+  geom_density(aes(fill = school.classification), color = NA, alpha = 0.5) +
+  scale_x_log10()
+
+# Elementary and secondary schools do not seem to exhibit difference,
+# so they will be treated as a whole entity.
 
